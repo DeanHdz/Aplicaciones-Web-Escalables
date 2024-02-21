819ead2ee5a5a4cd04e8c4f959fd06c686a403ee
DeanHdz <dean2103@gmail.com>
2024-02-21 16:53:26 -0600

    clase 29 de enero

--- proyecto1/src/app/app.component.ts
+++ proyecto1/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContadorComponent } from './contador/contador.component';
+import { ImagenComponent } from './imagen/imagen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContadorComponent, ImagenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

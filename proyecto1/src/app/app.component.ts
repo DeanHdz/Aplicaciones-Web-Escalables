543c09bb166835c991d9343cfc35c9ef5bb77d3f
DeanHdz <dean2103@gmail.com>
2024-02-21 17:16:17 -0600

    Ejercicio1

--- proyecto1/src/app/app.component.ts
+++ proyecto1/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContadorComponent } from './contador/contador.component';
 import { ImagenComponent } from './imagen/imagen.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContadorComponent, ImagenComponent],
+  imports: [RouterOutlet, ContadorComponent, ImagenComponent, HeaderComponent, FooterComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

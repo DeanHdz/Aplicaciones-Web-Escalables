505f257d2878b3dec992d5568d80fcfa5ba0f583
DeanHdz <dean2103@gmail.com>
2024-03-17 21:26:50 -0600

    Clase 4 de Marzo

--- rutas/src/app/app.routes.ts
+++ rutas/src/app/app.routes.ts
@@ -3,10 +3,17 @@ import { HomePage } from './pages/home/home.page';
 import { AboutPage } from './pages/about/about.page';
 import { ContactPage } from './pages/contact/contact.page';
 import { TvShowsPage } from './pages/tv-shows/tv-shows.page';
+import { ErrorPage } from './pages/error/error.page';
 
 export const routes: Routes = [
+    {path: "", redirectTo: "home", pathMatch: "full"},
+    
+    //{path: "**", component: ErrorPage}, //Cualquiera que no entre en las rutas anteriores automaticamente redirige a error
+    
     {path: "home", component: HomePage},
     {path: "about", component: AboutPage},
     {path: "contact", component: ContactPage},
     {path: "tvshow", component: TvShowsPage},
+    {path: "error", component: ErrorPage},
+    {path: "**", redirectTo: "error", pathMatch: "full"}, //Lo mismo que lo anterior pero mejor
 ];
